812bb451ad492724edfd287034a4375f3e2af527
Sowmiya310798 <151819284+Sowmiya310798@users.noreply.github.com>
2025-10-27 11:08:46 +0530

    update

--- src/dynamo/dynamo.service.ts
+++ src/dynamo/dynamo.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import {
   DynamoDBDocumentClient,
+  GetCommand,
   PutCommand,
   QueryCommand,
 } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { v4 as uuid } from 'uuid';
+
+export interface Chat {
+  chatId: string;
+  title: string;
+  createdAt: string;
+  lastMessageAt: string;
+  messageCount: number;
+}
 @Injectable()
 export class DynamoService {
   public readonly client: DynamoDBDocumentClient;
@@ -19,4 +29,92 @@ export class DynamoService {
     });
     this.client = DynamoDBDocumentClient.from(client);
   }
-}
+
+  //CHAT
+  async createChat(userId: string, title: string | 'New Chat'): Promise<Chat> {
+    const chatId = uuid();
+    const now = new Date().toISOString();
+    const item = {
+      PK: `USER#${userId}`,
+      SK: `CHAT#${chatId}`,
+      GSIPK: `USER#`,
+      userId,
+      chatId,
+      title,
+      createdAt: now,
+      lastMessageAt: now,
+      messageCount: 0,
+    };
+    await this.client.send(
+      new PutCommand({
+        TableName: 'bot',
+        Item: item,
+      }),
+    );
+    return item;
+  }
+
+  async getUserChats(userId: string, chatId: string) {
+    const res = await this.client.send(
+      new QueryCommand({
+        TableName: 'bot',
+        KeyConditionExpression: 'PK = :pk AND begins_with(SK, :sk)',
+        ExpressionAttributeValues: {
+          ':pk': `USER#${userId}`,
+          ':sk': `CHAT#${chatId}#MSG#`,
+        },
+      }),
+    );
+    return res.Items || [];
+  }
+
+  async getChat(userId:string,chatId:string){
+    const res=await this.client.send(new GetCommand({
+      TableName:'bot',
+      Key:{
+        PK:`USER#${userId}`,
+        SK:`CHAT#${chatId}`
+      }
+    }))
+    return res.Item
+  }
+
+  //MESSAGE
+  async saveChatMessage(
+    userId: string,
+    chatId: string,
+    role: string,
+    content: string,
+  ) {
+    const now = new Date().toISOString();
+    const item = {
+      PK: `USER#${userId}`,
+      SK: `CHAT#${chatId}#MSG#${now}`,
+      userId,
+      chatId,
+      role,
+      content,
+    };
+    await this.client.send(
+      new PutCommand({
+        TableName: 'bot',
+        Item: item,
+      }),
+    );
+    return item;
+  }
+
+  async getChatMessage(userId: string, chatId: string) {
+    const res = await this.client.send(
+      new QueryCommand({
+        TableName: 'bot',
+        KeyConditionExpression: 'PK = :pk AND begins_with(SK, :sk)',
+        ExpressionAttributeValues: {
+          ':pk': `USER#${userId}`,
+          ':sk': `CHAT#${chatId}#MSG#`,
+        },
+      }),
+    );
+    return res.Items || [];
+  }
+} 
